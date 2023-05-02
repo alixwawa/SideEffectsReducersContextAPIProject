f1a67970a09ede7bcf4ff1e3d59bf84a53c04071
Alix Wawa <alixwawa@yahoo.com>
2023-05-01 22:43:46 -0500

    added cart

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import classes from './Modal.module.css';
+
+const portalElement = document.getElementById('overlays');
+
+const Backdrop = (props) => {
+	return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+	return (
+		<div className={classes.modal}>
+			<div className={classes.content}>{props.children}</div>
+		</div>
+	);
+};
+
+
+const Modal = (props) => {
+	return (
+		<>
+			{ReactDom.createPortal(<Backdrop />, portalElement)}
+			{ReactDom.createPortal(
+				<ModalOverlay>{props.children}</ModalOverlay>,
+				portalElement,
+			)}
+		</>
+	);
+};
+
+export default Modal;
